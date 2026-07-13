e3673c6e186232310d38a739f9b12c2870ee8fbd
liyin <ahmadiliyin78@gmail.com>
2026-07-13 23:26:49 +0700

    replace yt-dlp package for deployment

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const cors = require('cors');
-const youtubedl = require('yt-dlp-exec');
+const youtubedl = require('youtube-dl-exec');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
